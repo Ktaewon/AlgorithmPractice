c6c0396c7ddee3befa63bd3c1d930ef547080c46
Ktaewon <dydalehdxo@naver.com>
2022-11-19 02:24:35 +0900

    FEAT: Binary Search is added

---
+++ BinarySearch/binarySearch.py
@@ -0,0 +1,25 @@
+def binarySearchRecursion(arr, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if arr[mid] == target:
+        return mid
+    # 중간점보다 작은 경우
+    elif arr[mid] < target:
+        return binarySearchRecursion(arr, target, start, mid - 1)
+    # 중간점보다 큰 경우
+    else:
+        return binarySearchRecursion(arr, target, mid + 1, end)
+
+def binarySearchIterative(arr, target, start, end)
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == target:
+            return mid
+        # 중간점보다 작은 경우
+        elif arr[mid] < target:
+            end = mid - 1
+        # 중간점보다 큰 경우
+        else:
+            start = mid + 1
+    return None
